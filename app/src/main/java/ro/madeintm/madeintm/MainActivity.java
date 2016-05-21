c6ada3acb5aca9e76f236a22db6435f44d6a859a
Vali Draganescu <vali.draganescu88@gmail.com>
2016-05-21 10:01:04 +0300

    first commit

---
+++ app/src/main/java/ro/madeintm/madeintm/MainActivity.java
@@ -0,0 +1,13 @@
+package ro.madeintm.madeintm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
